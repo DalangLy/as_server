769c4bd6ebe8fe3bd48746c228f1aee1f9166bcd
Dalang <dalangly@gmail.com>
2025-11-20 14:54:45 +0700

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to AS Server!';
+  }
+
+  @Post('login')
+  async login(username: string, password: string): Promise<boolean> {
+    return true;
   }
 }
