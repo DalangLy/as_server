270e9e104de7bf060c078c6a7befd4f6a8ea98be
Dalang <dalangly@gmail.com>
2025-11-20 15:05:21 +0700

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, Post } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from './users/users.service';
 
 @Injectable()
 export class AppService {
@@ -6,8 +7,16 @@ export class AppService {
     return 'Welcome to AS Server!';
   }
 
-  @Post('login')
-  async login(username: string, password: string): Promise<boolean> {
-    return true;
+  constructor(private usersService: UsersService) {}
+
+  async signIn(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return result;
   }
 }
